76150c7cfdb599baa34124f2e3a6bfeb025d5e76
Michael Guzman <mgzmn4@gmail.com>
2022-12-15 20:50:24 -0600

    Solution for day 3 part 1

---
+++ day3.py
@@ -0,0 +1,22 @@
+priority = {
+    'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5, 'f' : 6, 'g' : 7, 'h' : 8, 'i' : 9, 'j' : 10, 'k' : 11, 'l' : 12, 'm' : 13, 
+    'n' : 14, 'o' : 15, 'p' : 16, 'q' : 17, 'r' : 18, 's' : 19, 't' : 20, 'u' : 21, 'v' : 22, 'w' : 23, 'x' : 24, 'y' : 25, 'z' : 26,
+    'A' : 27, 'B' : 28, 'C' : 29, 'D' : 30, 'E' : 31, 'F' : 32, 'G' : 33, 'H' : 34, 'I' : 35, 'J' : 36, 'K' : 37, 'L' : 38, 'M' : 39,
+    'N' : 40, 'O' : 41, 'P' : 42, 'Q' : 43, 'R' : 44, 'S' : 45, 'T' : 46, 'U' : 47, 'V' : 48, 'W' : 49, 'X' : 50, 'Y' : 51, 'Z' : 52
+}
+
+def findCommon(first, second) :
+    for char1 in first :
+        for char2 in second :
+            if (char1 == char2) : 
+                return char1
+
+total = 0
+with open('input3.txt', 'r') as f:
+    for line in f:
+        content = line.strip()
+        first, second = content[:len(content)//2], content[len(content)//2:]
+        # find common char in each array and add to sum
+        total += priority[findCommon(first, second)]
+
+print(total)
